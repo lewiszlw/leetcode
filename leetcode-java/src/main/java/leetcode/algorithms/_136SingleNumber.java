ea98884b1971ced616f107f56b4d2c33ccc37e01
zhanglinwei02 <zhanglinwei02@meituan.com>
2019-03-27 23:43:25 +0800

    No.136 Single Number

--- leetcode-java/src/main/java/leetcode/algorithms/_136SingleNumber.java
+++ leetcode-java/src/main/java/leetcode/algorithms/_136SingleNumber.java
@@ -1,10 +1,28 @@
 package leetcode.algorithms;
 
+import org.junit.Test;
+
 /**
  * Desc:
+ * 136 SingleNumber
+ * 解决方法：异或。两个相同的数异或为0，0与任何数异或为该数不变，异或满足交换律、结合律。
  *
  * @author zhanglinwei02
  * @date 2019-03-27
  */
 public class _136SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {1,2,2,4,4};
+        System.out.println(singleNumber(nums));
+    }
+
 }
