4523e61ff3bb0a9073bf0a69112dfb26aa86a7a7
zhanglinwei02 <zhanglinwei02@meituan.com>
2019-06-16 12:49:38 +0800

    No.23 Merge k Sorted Lists

--- leetcode-java/src/main/java/leetcode/algorithms/_0023MergeKSortedLists.java
+++ leetcode-java/src/main/java/leetcode/algorithms/_0023MergeKSortedLists.java
@@ -43,6 +43,9 @@ package leetcode.algorithms;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Desc:
  *
@@ -89,6 +92,73 @@ public class _0023MergeKSortedLists {
         return head.next;
     }
 
+
+    /**
+     * 解法二：分治法
+     * 两两合并，然后合并结果继续两两合并，直至最后合并成一个链表
+     */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        if (lists.length == 1) {
+            return lists[0];
+        }
+
+        ListNode[] newLists = new ListNode[(lists.length + 1) / 2];
+        int index = 0;
+
+        int i = 0;
+        while (i < lists.length) {
+            ListNode root1 = lists[i ++];
+            ListNode root2 = null;
+            // 此时i可能超过length
+            if (i < lists.length) {
+                root2 = lists[i ++];
+            }
+            newLists[index ++] = mergeTwoSortedLists(root1, root2);
+        }
+        return mergeKLists2(newLists);
+    }
+
+    /**
+     * 合并两个有序链表
+     */
+    private ListNode mergeTwoSortedLists(ListNode root1, ListNode root2) {
+        if (root2 == null) {
+            return root1;
+        }
+        ListNode p1 = root1;
+        ListNode p2 = root2;
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        if (p1 != null) {
+            while (p1 != null) {
+                p.next = p1;
+                p = p.next;
+                p1 = p1.next;
+            }
+        }
+        if (p2 != null) {
+            while (p2 != null) {
+                p.next = p2;
+                p = p.next;
+                p2 = p2.next;
+            }
+        }
+        return head.next;
+    }
+
     class ListNode {
         int val;
         ListNode next;
@@ -115,6 +185,42 @@ public class _0023MergeKSortedLists {
         ListNode[] lists = new ListNode[] {p1, q1, k1};
 
         ListNode p = mergeKLists(lists);
+        Assert.assertTrue(p.val == 1);
+        p = p.next;
+        Assert.assertTrue(p.val == 1);
+        p = p.next;
+        Assert.assertTrue(p.val == 2);
+        p = p.next;
+        Assert.assertTrue(p.val == 3);
+        p = p.next;
+        Assert.assertTrue(p.val == 4);
+        p = p.next;
+        Assert.assertTrue(p.val == 4);
+        p = p.next;
+        Assert.assertTrue(p.val == 5);
+        p = p.next;
+        Assert.assertTrue(p.val == 6);
+    }
+
+    @Test
+    public void test2() {
+        ListNode p1 = new ListNode(1);
+        ListNode p2 = new ListNode(4);
+        ListNode p3 = new ListNode(5);
+        p1.next = p2; p2.next = p3;
+
+        ListNode q1 = new ListNode(1);
+        ListNode q2 = new ListNode(3);
+        ListNode q3 = new ListNode(4);
+        q1.next = q2; q2.next = q3;
+
+        ListNode k1 = new ListNode(2);
+        ListNode k2 = new ListNode(6);
+        k1.next = k2;
+
+        ListNode[] lists = new ListNode[] {p1, q1, k1};
+
+        ListNode p = mergeKLists2(lists);
         Assert.assertTrue(p.val == 1);
         p = p.next;
         Assert.assertTrue(p.val == 1);
